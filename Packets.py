1fa5dd2180f2af740de34bfe2390370ecd6e87f4
MaciejPurski <mpurski@mion.elka.pw.edu.pl>
2018-03-30 22:15:17 +0200

    serialize packets

--- Packets.py
+++ Packets.py
@@ -1,3 +1,6 @@
+import struct
+
+
 class PacketType:
 	ACK = 0x01
 	NAK = 0x02
@@ -12,67 +15,57 @@ class PacketType:
 
 
 class Packet:
-	def __init__(self, pid, p_len, p_format_str):
+	def __init__(self, pid, p_len, format_str):
 		self.pid = pid
 		self.p_len = p_len
 		self.p_len_encrypted = (p_len / 16 + 1) * 6  # AES encrypted message length
-		self.p_format_str = p_format_str
+		self.format_str = format_str
+		self._values_tuple = ()
+
+	def deserialize(self, buffer):
+		self._values_tuple = struct.unpack(buffer, self.format_str)
+		#TODO error check
+
+	def serialize(self):
+		return struct.pack(self.format_str, self._values_tuple)
 
 
 class PacketACK(Packet):
 	def __init__(self):
-		super().__init__(PacketType.ACK, 1, '')
-		self.service_id = 0
+		super().__init__(PacketType.ACK, 1, 'b')
 
 	def set_fields(self, service_id):
 		if not isinstance(service_id, int):
 			raise TypeError('service_id should be int')
 
-		self.service_id = service_id
+		super()._values_tuple = (service_id, )
 
-	def deserialize(self, buffer):
-		pass
-
-	def serialize(self):
-		return bytearray('')
+	def packet_id(self):
+		return super()._values_tuple[0]
 
 
 class PacketNAK(Packet):
-
 	def __init__(self):
-		super().__init__(PacketType.NAK, 1, '')
-		self.service_id = 0
+		super().__init__(PacketType.NAK, 1, 'b')
 
 	def set_fields(self, service_id):
 		if not isinstance(service_id, int):
 			raise TypeError('service_id should be int')
 
-		self.service_id = service_id
+		super()._values_tuple = (service_id,)
 
-	def serialize(self):
-		return bytearray('')
-
-	def deserialize(self, buffer):
-		pass
+	def packet_id(self):
+		return super()._values_tuple[0]
 
 
 class PacketEOT(Packet):
-
 	def __init__(self):
 		super().__init__(PacketType.EOT, 0, '')
 
-	def serialize(self):
-		return bytearray('')
-
-	def deserialize(self, buffer):
-		pass
-
 
 class PacketCHALL(Packet):
-
 	def __init__(self):
 		super().__init__(PacketType.CHALL, 8, '')
-		self.random_bytes = bytes('')
 
 	def set_fields(self, random_bytes):
 		if not isinstance(random_bytes, bytes):
@@ -81,19 +74,16 @@ class PacketCHALL(Packet):
 		if len(random_bytes) != 8:
 			raise ValueError('random bytes len should be equal 8')
 
-		self.random_bytes = random_bytes
+		super()._values_tuple = (random_bytes, )
 
-	def serialize(self):
-		return bytearray('')
-
-	def deserialize(self, buffer):
-		pass
+	def random_bytes(self):
+		return super()._values_tuple[0]
 
 
 class PacketCHALL_RESP(Packet):
+
 	def __init__(self):
-		super().__init__(PacketType.CHALL_RESP, 256, '')
-		self.encrypted_bytes = bytes('')
+		super().__init__(PacketType.CHALL_RESP, 256, '8c')
 
 	def set_fields(self, encrypted_bytes):
 		if not isinstance(encrypted_bytes, bytes):
@@ -102,18 +92,15 @@ class PacketCHALL_RESP(Packet):
 		if len(encrypted_bytes) != 256:
 			raise ValueError('encrypted bytes len should be equal 256')
 
-		self.encrypted_bytes = encrypted_bytes
+		super()._values_tuple = (encrypted_bytes, )
 
-	def serialize(self):
-		return bytearray('')
-
-	def deserialize(self, buffer):
-		pass
+	def encrypted_bytes(self):
+		return super()._values_tuple[0]
 
 
 class PacketKEY(Packet):
 	def __init__(self):
-		super().__init__(PacketType.KEY, 16, '')
+		super().__init__(PacketType.KEY, 16, '16c')
 		self.symetric_key = bytes('')
 
 	def set_fields(self, symetric_key):
@@ -123,23 +110,12 @@ class PacketKEY(Packet):
 		if len(symetric_key) != 8:
 			raise ValueError('random bytes len should be equal 8')
 
-		self.symetric_key = symetric_key
-
-	def serialize(self):
-		return bytearray('')
-
-	def deserialize(self, buffer):
-		pass
+		super()._values_tuple = (symetric_key, )
 
 
 class PacketDESC(Packet):
 	def __init__(self):
-		super().__init__(PacketType.DESC, 76, '')
-		self.dev_class = 0
-		self.name = bytes('')
-		self.unit = bytes('')
-		self.min_value = 0.0
-		self.max_value = 0.0
+		super().__init__(PacketType.DESC, 77, '1b 64s 4s f f')
 
 	def set_fields(self, dev_class, name, unit, min_value, max_value):
 		if not isinstance(dev_class, int) or not isinstance(name, bytes)\
@@ -154,77 +130,65 @@ class PacketDESC(Packet):
 		if len(unit) > 4:
 			raise ValueError('wrong unit value')
 
-		self.dev_class = dev_class
-		self.name = name
-		self.unit = unit
-		self.min_value = min_value
-		self.max_value = max_value
+		super()._values_tuple = (dev_class, name, unit, min_value, max_value)
 
-	def serialize(self):
-		return bytearray('')
+	def dev_class(self):
+		return super()._values_tuple[0]
 
-	def deserialize(self, buffer):
-		pass
+	def name(self):
+		return super()._values_tuple[1]
+
+	def unit(self):
+		return super()._values_tuple[2]
+
+	def min_value(self):
+		return super()._values_tuple[3]
+
+	def max_value(self):
+		return super()._values_tuple[4]
 
 
 class PacketVAL(Packet):
 	def __init__(self):
-		super().__init__(PacketType.VAL, 9, '')
-		self.service_id = 0
-		self.value = 0.0
-		self.timestamp = 0
+		super().__init__(PacketType.VAL, 9, 'b f 4I')
 
 	def set_fields(self, service_id, value, timestamp):
 		if not isinstance(service_id, int) or not isinstance(value, value)\
 				or not isinstance(timestamp, int):
 			raise TypeError('wrong argument type')
 
-		self.service_id = service_id
-		self.value = value
-		self.timestamp = timestamp
+		super()._values_tuple = (service_id, value, timestamp)
 
-	def serialize(self):
-		return bytearray('')
+	def service_id(self):
+		return super()._values_tuple[0]
 
-	def deserialize(self, buffer):
-		pass
+	def value(self):
+		return super()._values_tuple[1]
+
+	def timestamp(self):
+		return super()._values_tuple[2]
 
 
 class PacketSET(Packet):
 	def __init__(self):
-		super().__init__(PacketType.SET, 5, '')
-		self.service_id = 0
-		self.value = 0.0
+		super().__init__(PacketType.SET, 5, 'b f')
 
 	def set_fields(self, service_id, value):
 		if not isinstance(service_id, int) or not isinstance(value, value):
 			raise TypeError('wrong argument type')
-		self.service_id = service_id
-		self.value = value
 
-	def serialize(self):
-		return bytearray('')
-
-	def deserialize(self, buffer):
-		pass
+		super()._values_tuple = (service_id, value)
 
 
 class PacketEXIT(Packet):
 	def __init__(self):
-		super().__init__(PacketType.EXIT, 1, '')
-		self.service_id = 0
+		super().__init__(PacketType.EXIT, 1, 'b')
 
 	def set_fields(self, service_id):
 		if not isinstance(service_id, int):
 			raise TypeError('wrong argument type')
 
-		self.service_id = service_id
-
-	def serialize(self):
-		return bytearray('')
-
-	def deserialize(self, buffer):
-		pass
+		super()._values_tuple = (service_id, )
 
 
 def packet_factory(pid):
--- ProtocolLayer.py
+++ ProtocolLayer.py
@@ -3,6 +3,7 @@ import Packets
 from Packets import packet_factory
 from CryptoCore import CipherRSA
 
+
 class Protocol:
 	def __init__(self, server_ip, port, public_key, private_key):
 		self.cipher_rsa = CipherRSA(public_key, private_key)
@@ -14,14 +15,14 @@ class Protocol:
 		if not isinstance(rcv_packet, Packets.PacketCHALL):
 			raise TypeError('wrong packet received')
 
-		encrypted_bytes = CipherRSA.encrypt_rsa(rcv_packet.random_bytes)
+		encrypted_bytes = CipherRSA.encrypt_rsa(rcv_packet.random_bytes())
 		chall_resp = packet_factory(Packets.PacketType.CHALL_RESP)
 		chall_resp.set_fields(encrypted_bytes)
 
 		self.connection.packet_send(chall_resp, False)
-
-
+		
 
 
 	def register_seq(self, descriptorsList):
 		self.connection.establish_connection()
+		self.authenticate()
